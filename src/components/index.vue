3b8b6806cc11db06ae06f607ec53dac1906bb758
funnycoderstar <funnycoderstar@gmail.com>
2018-09-21 23:13:03 +0800

    feat: 修改首页样式

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,9 +1,9 @@
 <template>
     <div>
         <ul class="menu">
-            <li @click="handleCalendar">1,日历组件demo</li>
-            <li @click="handleTime">2,时间选择组件demo</li>
-            <li @click="handleSelect">3,selectDemo</li>
+            <li @click="handleCalendar">1.日历组件demo</li>
+            <li @click="handleTime">2.时间选择组件demo</li>
+            <li @click="handleSelect">3.selectDemo</li>
             <!-- <li @click="handleSelect">4,tabDemo</li> -->
         </ul>
     </div>
@@ -31,5 +31,8 @@ export default {
 .menu {
     .xtr(line-height, 40);
     .xtr(padding, 20);
+    li {
+        cursor: pointer;
+    }
 }
 </style>
